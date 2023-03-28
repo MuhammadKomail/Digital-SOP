e5989b9b17c4950df74e4e06fe7d805457cddc29
= <hassanshabbir202@gmail.com>
2023-03-28 13:53:51 -0700

    add permission

--- src/Screens/Settings/index.js
+++ src/Screens/Settings/index.js
@@ -2,6 +2,7 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import {View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
+import VideoPlayer from "react-native-video-player"
 
 export const Settings = () => {
   const Navigation = useNavigation()
@@ -12,10 +13,18 @@ export const Settings = () => {
         alignSelf: 'center',
         justifyContent: 'center',
       }}>
+       
+       <VideoPlayer
+    video={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+    // videoWidth={1600}
+    // videoHeight={900}
+    // thumbnail={{ uri: 'https://i.picsum.photos/id/866/1600/900.jpg' }}
+/>
+
       <Button
         mode="contained"
         onPress={() => Navigation.navigate('Profile')}>
-        Go to Profile
+        Go to Prof
       </Button>
     </View>
   );
